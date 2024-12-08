3d09202889986a6f500fe4a6d4d1b614483d7112
penglingwei <penglingwei@zju.edu.cn>
2024-12-08 15:27:31 +0800

    generate deps

--- extract_thms.py
+++ extract_thms.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import zipfile
+import shutil
 
 from huggingface_hub import HfApi
 from huggingface_hub.utils import RepositoryNotFoundError
@@ -8,18 +9,19 @@ from tqdm import tqdm
 
 import subprocess
 import sys
+import re
 
-def run_lean_script(thmsfile_path: str, output_dir: str, start_thm_idx: int|None = None, end_thm_idx: int|None = None):
+def run_lean_script(thmsfile: str, start_thm_idx: int|None = None, end_thm_idx: int|None = None, max_prop_size=2048, max_proof_size=10000):
   try:
     if start_thm_idx is None:
       start_thm_idx = 0
     if end_thm_idx is None:
-      with open(thmsfile_path, 'r') as f:
+      with open(f"{thmsfile}.txt", 'r') as f:
         end_thm_idx = len(f.readlines())
 
     # 启动 Lean 子进程
     process = subprocess.Popen(
-      ["lake", "env", "lean", "--run", "MathlibInspector.lean", thmsfile_path, output_dir, start_thm_idx, end_thm_idx],
+      ["lake", "env", "lean", "--run", "MathlibInspector.lean", f"{thmsfile}.txt", thmsfile, start_thm_idx, end_thm_idx, max_prop_size, max_proof_size],
         stdout=sys.stdout,  # 子进程的 stdout 重定向到父进程
         stderr=sys.stderr,  # 子进程的 stderr 重定向到父进程
         text=True
@@ -37,19 +39,6 @@ def run_lean_script(thmsfile_path: str, output_dir: str, start_thm_idx: int|None
   except Exception as e:
     print(f"An unexpected error occurred: {e}")
 
-def hash_string(s: str) -> str:
-  modulus = 4294967311 
-  rst = 5381
-  multiplier = 131
-  for ch in s:
-    rst = (rst * multiplier + ord(ch)) % modulus
-  return str(rst)  # 转换为16进制字符串，去掉 '0x' 前缀
-
-def extractPrefix(s: str) -> str :
-  if s.startswith("Lean."):
-    return s[5:7]
-  return s[:2]
-
 def zip_dataset(dataset_dir, output_zip):
   file_list = []  # 创建文件列表
   for root, dirs, files in os.walk(dataset_dir):
@@ -61,6 +50,68 @@ def zip_dataset(dataset_dir, output_zip):
     for file_path in tqdm(file_list, desc="压缩中", unit="文件"):  # 添加进度条
       zipf.write(file_path, os.path.relpath(file_path, dataset_dir))
 
+def upload(thmsfile: str, end_of_index: int):
+  with open(f"{thmsfile}.txt", 'r') as f:
+    thms_total_num = len(f.readlines())
+  
+  if end_of_index is None or end_of_index <= start_of_index:
+    end_of_index = thms_total_num
+
+  # zip output_dir
+  output_zip = thmsfile + '-' + str(start_of_index) + '-' + str(end_of_index) + ".zip"
+  zip_dataset(thmsfile, output_zip)
+
+  # 上传数据集到 Hugging Face
+  api = HfApi()
+  repo_id = args.repo_id
+  path_in_repo = output_zip
+
+  try:
+    api.dataset_info(repo_id)
+    print(f"数据集 {repo_id} 已存在。")
+  except RepositoryNotFoundError:
+    print(f"数据集 {repo_id} 不存在，正在创建...")
+    api.create_repo(repo_id, repo_type="dataset")
+
+  # 通过 upload_with_progress 进行直接上传
+  with open(output_zip, "rb") as f:  # 以二进制模式打开文件
+    # 进行上传
+    try:
+      api.upload_file(
+        path_or_fileobj=f,  # 传递文件对象
+        path_in_repo=path_in_repo,
+        repo_id=repo_id,
+        repo_type="dataset",
+      )
+      print(output_zip, "上传成功")  # 上传成功提示
+    except Exception as e:
+      print(f"上传失败: {e}")
+
+def extract_names_from_file(filepath):
+  pattern = r"\(C (\w+)\)"
+  names = []
+  with open(filepath, 'r', encoding='utf-8') as file:
+    for line in file:
+      names.extend(re.findall(pattern, line))
+  return names
+
+def load_thms(thmfile):
+  thms = []
+  with open(f"{thmfile}.txt", "r") as f:
+    thms = [line.strip() for line in f.readlines()]
+  return thms
+
+def get_ext_depth(previousThms: set[str], thmsfile: str):
+  deps = set() # 证明依赖的常量 
+  for thm_file in tqdm(thmsfile):
+    names = extract_names_from_file(os.path.join(thmsfile, f"{thm_file}.txt"))
+    for name in names:
+      if name not in previousThms:
+        deps.add(name)
+  depslist = list(deps)
+  depslist.sort()
+  return depslist
+
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description="Upload dataset")
   parser.add_argument(
@@ -69,13 +120,6 @@ if __name__ == "__main__":
     dest="thmsfile",
     type=str,
     help="Theorems file path",
-  )
-  parser.add_argument(
-    "-o",
-    "--output",
-    dest="output",
-    type=str,
-    help="Output folder",
   )
   parser.add_argument(
     "-d",
@@ -108,73 +152,60 @@ if __name__ == "__main__":
     help="Enable generator of lean4 script",
     default=False,
   )
+  parser.add_argument(
+    "--max-prop-size",
+    dest="max_prop_size",
+    type=int,
+    help="Max Prop Size",
+    default=2048,
+  )
+  parser.add_argument(
+    "--max-proof-size",
+    dest="max_proof_size",
+    type=int,
+    help="Max Proof Size",
+    default=100000,
+  )
+  parser.add_argument(
+    "--depth",
+    dest="depth",
+    type=int,
+    help="Depth",
+    default=0
+  )
 
   args = parser.parse_args()
 
-  thmsfile_path = args.thmsfile
-  output_dir = args.output
+  thmsfile = args.thmsfile
   start_of_index = args.start
   end_of_index = args.end
+  max_prop_size = args.max_prop_size
+  max_proof_size = args.max_proof_size
 
   if args.generate:
-    run_lean_script(thmsfile_path, output_dir, start_of_index, end_of_index)
+    run_lean_script(thmsfile, start_of_index, end_of_index, max_prop_size, max_proof_size)
+
+  upload(thmsfile, end_of_index)
   
-  with open("thms.txt", 'r') as f:
-    thms_total_num = len(f.readlines())
-  
-  if end_of_index is None or end_of_index <= start_of_index:
-    end_of_index = thms_total_num
+  depth = args.depth
+  if depth == 0 or args.generate:
+    exit(0)
 
-  # zip output_dir
-  output_zip = output_dir + '-' + str(start_of_index) + '-' + str(end_of_index) + ".zip"
-  zip_dataset(output_dir, output_zip)
-
-  # 上传数据集到 Hugging Face
-  api = HfApi()
-  repo_id = args.repo_id
-  path_in_repo = output_zip
-
-  try:
-    api.dataset_info(repo_id)
-    print(f"数据集 {repo_id} 已存在。")
-  except RepositoryNotFoundError:
-    print(f"数据集 {repo_id} 不存在，正在创建...")
-    api.create_repo(repo_id, repo_type="dataset")
-
-  # 通过 upload_with_progress 进行直接上传
-  with open(output_zip, "rb") as f:  # 以二进制模式打开文件
-    # 进行上传
-    try:
-      api.upload_file(
-        path_or_fileobj=f,  # 传递文件对象
-        path_in_repo=path_in_repo,
-        repo_id=repo_id,
-        repo_type="dataset",
-      )
-      print(output_zip, "上传成功")  # 上传成功提示
-    except Exception as e:
-      print(f"上传失败: {e}")
-
-  """
-  const_hash_list: list[str] = []
-
-  with open("consts.txt", "r") as f:
-    for line in tqdm(f.readlines()):
-      line = line.strip()
-      head = extractPrefix(line)
-      body = hash_string(line)
-      code = head + body
-      const_hash_list.append(f"{code}\t{line}\n")
-  
-  with open("hashed_consts.txt", "w") as f:
-    f.writelines(const_hash_list)
-
-  words_path = "hashed_consts.txt"
-  api.upload_file(
-    path_or_fileobj=words_path,
-    path_in_repo=words_path,
-    repo_id=repo_id,
-    repo_type="dataset",
-  )
-  print(words_path, "上传成功") 
-  """
+  previousThms = load_thms(thmsfile)
+  for idx in range(depth):
+    previous_thmsfile = f"{thmsfile}_dep{idx-1}" if idx > 0 else thmsfile
+    next_thmsfile = f"{thmsfile}_dep{idx}"
+    deps = get_ext_depth(previousThms, previous_thmsfile)
+    if len(deps) == 0:
+      break
+    shutil.rmtree(previous_thmsfile)
+    print(f"开始写入thms_dep1={len(deps)}...")
+    with open(f"{next_thmsfile}.txt", "r") as f:
+      f.writelines([line + "\n" for line in deps])
+    print("成功写入thms_dep1")
+    os.system(f"git add {next_thmsfile}.txt")
+    os.system(f'git commit -m "add {next_thmsfile}.txt"')
+    os.system("git push origin main")
+    run_lean_script(next_thmsfile, 0, None, max_prop_size, max_proof_size)
+    upload(next_thmsfile, None)
+    
--- tests.lean
+++ tests.lean
@@ -13,43 +13,9 @@ open Lean Meta
 -- #eval saveThmListToFile "thms.txt"
 -- #eval saveConstAndSizeToFile "constAndSize.txt" 2048 100000
 
-#eval printConstantDetails `eq_iff_eq_cancel_left
-
-#check @Prod
-#eval printConstantDetails `Prod.fst
+#eval printConstantDetails `tsum_nonneg
 
 #eval printConstantDetails `Iff
 #check @Iff.intro
 #eval printConstantDetails `Iff.refl
 #eval printConstantDetails `Iff.intro
-
-#check RingHom.IsIntegral.quotient
-#eval printConstantDetails `RingHom.IsIntegral.quotient
-#check Asymptotics.superpolynomialDecay_iff_isBigO
-
-#print CommRing
-
-def e1 : ∃ x : Nat, x > 0 :=
-  have h : 1 > 0 := Nat.zero_lt_succ 0
-  ⟨1, h⟩
-
-#check @e1
-#eval printConstantDetails `e1
-#eval printConstantDetails `Exists
-#eval printConstantDetails `Nat
-#eval printConstantDetails `GT.gt
-#eval printConstantDetails `instLTNat
-#eval printConstantDetails `OfNat.ofNat
-#eval printConstantDetails `instOfNatNat
-#eval printConstantDetails `letFun
-#eval printConstantDetails `Nat.zero_lt_succ
-#eval printConstantDetails `Exists.intro
-
-#check LT
-#eval printConstantDetails `LT
-#check @LT.lt
-#eval printConstantDetails `LT.lt
-
-
-#check Expr.lit
-#check @Literal.natVal
